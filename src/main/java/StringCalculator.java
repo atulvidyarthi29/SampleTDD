81347314739d4e129c8307647d0101d05b8c5cb9
atulvidyarthi29 <atulvidyarthi29@gmail.com>
2021-02-16 03:11:03 +0530

    till add two numbers testcase

---
+++ src/main/java/StringCalculator.java
@@ -0,0 +1,6 @@
+public class StringCalculator {
+    public int add(String str) {
+        if (str.length() == 0) return 0;
+        return Integer.parseInt(str);
+    }
+}
---
+++ src/test/java/StringCalculatorTest.java
@@ -0,0 +1,23 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StringCalculatorTest {
+
+    @Test
+    public void testEmptyString() {
+        StringCalculator stringCalculator = new StringCalculator();
+        Assert.assertEquals(0, stringCalculator.add(""));
+    }
+
+    @Test
+    public void testStringWithOneNumber(){
+        StringCalculator stringCalculator = new StringCalculator();
+        Assert.assertEquals(1, stringCalculator.add("1"));
+    }
+
+//    @Test
+//    public void testStringWithTwoNumber() {
+//        StringCalculator stringCalculator = new StringCalculator();
+//        Assert.assertEquals(11, stringCalculator.add("5,6"));
+//    }
+}
